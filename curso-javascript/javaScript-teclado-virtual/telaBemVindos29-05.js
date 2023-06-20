b7b55c38c2f002e21552a4f46e5996399bae39d2
3jv <3jvsenai@gmail.com>
2023-06-19 21:25:51 -0300

    salvar

--- curso-javascript/javaScript-teclado-virtual/telaBemVindos29-05.js
+++ curso-javascript/javaScript-teclado-virtual/telaBemVindos29-05.js
@@ -3,45 +3,45 @@ let botao = document.getElementById("botao");
 let nomeCadastro = []; // Inicializa um array vazio para armazenar os nomes de usuário cadastrados
 
 function entrar() {
-    let encontrou = false;
-    let nome = prompt("Digite o seu nome");
+  let encontrou = false;
+  let nome = prompt("Digite o seu nome");
 
-    if (nome === '' || nome === null) {
-        alert("Ops, algo deu errado!");
-        area.innerHTML = "Clique no botão para acessar.";
-    } else {
-        for (let i = 0; i < nomeCadastro.length; i++) {
-            if (nomeCadastro[i] === nome) {
-                area.innerHTML = "Bem-vindo(a), " + nome + "<br>";
-                let botaoSair = document.createElement("button");
-                botaoSair.innerText = "Sair da conta";
-                botaoSair.onclick = sair;
-                area.appendChild(botaoSair);
-                botao.setAttribute("hidden", "true");
-                encontrou = true;
-                return;
-            }
-        }
+  if (nome === '' || nome === null) {
+    alert("Ops, algo deu errado!");
+    area.innerHTML = "Clique no botão para acessar.";
+  } else {
+    for (let i = 0; i < nomeCadastro.length; i++) {
+      if (nomeCadastro[i] === nome) {
+        area.innerHTML = "Bem-vindo(a), " + nome + "<br>";
+        let botaoSair = document.createElement("button");
+        botaoSair.innerText = "Sair da conta";
+        botaoSair.onclick = sair;
+        area.appendChild(botaoSair);
+        botao.setAttribute("hidden", "true");
+        encontrou = true;
+        return;
+      }
     }
-    if (!encontrou) {
-        alert("Nome não cadastrado!");
-    }
+  }
+  if (!encontrou) {
+    alert("Nome não cadastrado!");
+  }
 }
 
 function sair() {
-    alert("Até mais!");
-    area.innerHTML = "Você saiu.";
-    botao.removeAttribute("hidden");
+  alert("Até mais!");
+  area.innerHTML = "Você saiu.";
+  botao.removeAttribute("hidden");
 }
 
 function cadastrarUsuario() {
-    let novoUsuario = prompt("Digite seu nome");
+  let novoUsuario = prompt("Digite seu nome");
 
-    if (novoUsuario === '' || novoUsuario === null) {
-        alert('Ops, algo deu errado!');
-        area.innerHTML = "Clique no botão para cadastrar novamente.";
-    } else {
-        nomeCadastro.push(novoUsuario);
-        alert("Cadastro realizado com sucesso!");
-    }
+  if (novoUsuario === '' || novoUsuario === null) {
+    alert('Ops, algo deu errado!');
+    area.innerHTML = "Clique no botão para cadastrar novamente.";
+  } else {
+    nomeCadastro.push(novoUsuario);
+    alert("Cadastro realizado com sucesso!");
+  }
 }
